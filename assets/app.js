b072c8c16b343c0025f6eedb12b21d86d6aecc2a
toonette88 <156116373+toonette88@users.noreply.github.com>
2024-12-01 16:19:15 +0100

    Finalisation de la page d'accueil

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
@@ -5,6 +6,19 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.scss';
+import './styles/app.css';
+
+import { Tooltip, Popover } from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Activer les tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...tooltipTriggerList].forEach(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
+
+    // Activer les popovers
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    [...popoverTriggerList].forEach(popoverTriggerEl => new Popover(popoverTriggerEl));
+});
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
